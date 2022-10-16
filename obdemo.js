0453b9112acae514b12b4f124bc5be10e0da1d91
eskieclunge <45446627+eskieclunge@users.noreply.github.com>
2022-10-16 16:32:51 +0300

    Charity update

--- obdemo.js
+++ obdemo.js
@@ -1,5 +1,5 @@
 function sendgbp() {
-    var x = Math.random() * 10000;
+    var x = (Math.floor(Math.random() * 100000000) + 100000000).toString().substring(1);
     var Amount = document.getElementById("Amount").value;
 
 
@@ -13,7 +13,7 @@ function sendgbp() {
             "countries":["GB"],
 
             "actingAs":{
-                "displayName": "Wolf Merchant",
+                "displayName": "Unicef UK",
                 "refId": "f43f2be7-5b21-4408-b0d4-63d2ba1ed3f3"
             },
     
@@ -26,11 +26,11 @@ function sendgbp() {
                     "transferDestinations": [
                         {
                             "fasterPayments": {
-                                "sortCode": "517014",
-                                "accountNumber": "78813166"
+                                "sortCode": "400602",
+                                "accountNumber": "91566814"
                             },
                             "customerData": {
-                                "legalNames": ["Peter Barnett"]
+                                "legalNames": ["Unicef"]
                             }
                         }
                     ]
@@ -39,7 +39,7 @@ function sendgbp() {
     
           
     
-            "description": "Donation-Saturdays",
+            "description": "Donation to Unicef",
             "refId": x,
             "redirectUrl": "https://pbtoken.azurewebsites.net/callback.html" // the URL that the request or PSU should be returned to (the TPP) 
             
@@ -64,7 +64,67 @@ function sendgbp() {
 }
 
 function sendeur() {
-    var x = Math.random() * 10000;
+    var x = (Math.floor(Math.random() * 100000000) + 100000000).toString().substring(1);
+    var Amount = document.getElementById("Amount").value;
+
+
+      const json = {
+        "requestPayload": {
+            "to": {
+                "alias": {"realmId":"m:jH8SCQjtW9uGEQCrNS99qCJ8cHN:5zKtXEAq","type":"DOMAIN","value":"pbarnett.ioo"},
+                "id": "m:2rEti5xfXPX13F7fsSpgU9FdX7ws:5zKtXEAq"
+            },
+    
+            "actingAs":{                              // Used to specify the sub-tpp executing the transaction
+                "refId": "f43f2be7-5b21-4408-b0d4-63d2ba1ed3f3",
+                "displayName": "Developer"
+    
+            },
+    
+
+            "transferBody": {
+                "currency": "EUR",
+                "lifetimeAmount": "1.20",
+                "instructions": {
+                    "transferDestinations": [
+                        {
+                            "sepa": {
+                                "iban": "GB63REVO00997010707063"    
+                            },
+                            "customerData": {
+                                "legalNames": ["King Pedro"]
+                            }
+                        }
+                    ]
+                }
+            },
+    
+          
+    
+            "description": "Example description",
+            "refId": x,
+            "redirectUrl": "http://pbtoken.azurewebsites.net/callback.html" // the URL that the request or PSU should be returned to (the TPP) 
+            
+        }
+    
+    };
+    
+
+    fetch("https://api.token.io/token-requests", {
+        method: "POST",
+        headers: {
+            "Authorization" : "Basic bS0yckV0aTV4ZlhQWDEzRjdmc1NwZ1U5RmRYN3dzLTV6S3RYRUFxOjlhODU0ZmJhLTAyMTYtNDg5Zi04ZWJlLWY5OTBlNjMyNTM3Yg==",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(json),
+    })
+    .then((response) => response.json())
+    .then((data) => renderrq(data));
+
+}
+
+function sendpln() {
+    var x = (Math.floor(Math.random() * 100000000) + 100000000).toString().substring(1);
     var Amount = document.getElementById("Amount").value;
 
 
@@ -78,22 +138,27 @@ function sendeur() {
             //"countries":["GB"],
 
             "actingAs":{
-                "displayName": "Wolf Merchant",
+                "displayName": "Polish Humanitarian Action",
                 "refId": "f43f2be7-5b21-4408-b0d4-63d2ba1ed3f3"
             },
     
             "transferBody": {
-                "currency": "EUR",
+                "currency": "PLN",
                 "returnRefundAccount": "true",
                 "lifetimeAmount": Amount,
                 "instructions": {
                     "transferDestinations": [
                         {
-                            "sepa": {
-                                "iban": "GB63REVO00997010707063"
+                            "elixir": {
+                                "accountNumber": "02249000050000460083168772" //CREDITOR IBAN IS MANDATORY
                             },
                             "customerData": {
-                                "legalNames": ["Peter Barnett"]
+                                "legalNames": [
+                                    "Polish Humanitarian Action" //CREDITOR NAME IS MANDATORY
+                                ],
+                                "address": {
+                                    "full": "tbc"
+                                }
                             }
                         }
                     ]
@@ -101,7 +166,7 @@ function sendeur() {
             },
           
     
-            "description": "Donation-Saturdays",
+            "description": "Donation to PAH",
             "refId": x,
             "redirectUrl": "https://pbtoken.azurewebsites.net/callback.html" // the URL that the request or PSU should be returned to (the TPP) 
             
@@ -124,6 +189,7 @@ function sendeur() {
     .then((data) => renderrq(data));
 
 }
+
 
 function renderrq(data) {
     // Get text elements
