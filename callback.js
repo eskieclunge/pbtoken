0880a2d9801456262a3408d8086a47f88c1c6f22
eskieclunge <45446627+eskieclunge@users.noreply.github.com>
2024-01-15 18:43:31 +0200

    Callback page fixed

--- callback.js
+++ callback.js
@@ -1,29 +1,39 @@
 
 
-function GetURLParameter(sParam){
-    var sPageURL = window.location.search.substring(1);
-    var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++) 
-    {
-        var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == sParam) 
-        {
-            return sParameterName[1];
-        }
+    function GetURLParameter(sParam){
+      var sPageURL = window.location.search.substring(1);
+      var sURLVariables = sPageURL.split('&');
+      for (var i = 0; i < sURLVariables.length; i++) 
+      {
+          var sParameterName = sURLVariables[i].split('=');
+          if (sParameterName[0] == sParam) 
+          {
+              return sParameterName[1];
+          }
+      } 
     } 
-  } 
+    let rq = GetURLParameter('request-id');
+    let status = GetURLParameter('transfer-status');
+    let t = GetURLParameter('transfer-id');
+    let errorMsg = GetURLParameter('message');
+
+
+    rq = decodeURIComponent(rq);
+    t = decodeURIComponent(t);
+    errorMsg = decodeURIComponent(errorMsg);
 
 
 
-  function renderrq(data) {
+
+    function renderrq(data) {
     // Get text elements
-  
+
       document.getElementById("rq").innerHTML = "requestId : " + rq;
-      document.getElementById("status").innerHTML = "Initial Payment Initiation Status : " + status;
+      document.getElementById("status").innerHTML = "First Payment Initiation Status : " + status;
       document.getElementById("t").innerHTML = "transferId : " + t;
       document.getElementById("errorMsg").innerHTML = "Error Message : " + errorMsg;
 
-      trStatus.innerHTML = "Payment Initiation Request Status : " + data.status;
+      trStatus.innerHTML = "Token Request Status : " + data.status;
       console.log(data.status);
 
     } 
@@ -32,7 +42,7 @@ function GetURLParameter(sParam){
       fetch("https://api.token.io/token-requests/"+rq+"/result", {
         method: "GET",
         headers: {
-            "Authorization" : "Basic bS0yckV0aTV4ZlhQWDEzRjdmc1NwZ1U5RmRYN3dzLTV6S3RYRUFxOjlhODU0ZmJhLTAyMTYtNDg5Zi04ZWJlLWY5OTBlNjMyNTM3Yg==",
+            "Authorization" : "Basic bS0yckV0aTV4ZlhQWDEzRjdmc1NwZ1U5RmRYN3dzLTV6S3RYRUFxOjM2YTExYzJmLWU5ZWItNGNlZi1iYzk1LTYyZDZiZjEzM2Y0Nw==",
             "Content-Type": "application/json",
         }
       })
@@ -43,19 +53,22 @@ function GetURLParameter(sParam){
 
     function rendert(data) {
     // Get text elements
-  
 
-      tStatus.innerHTML = "Polled Payment Initiation Transaction Status : " + data.transfer.status;
+
+      tStatus.innerHTML = "Polled Payment Initiation Status : " + data.transfer.status;
       console.log(data.transfer.status);
     } 
     function refreshT() {
       fetch("https://api.token.io/transfers/"+t, {
         method: "GET",
         headers: {
-            "Authorization" : "Basic bS0yckV0aTV4ZlhQWDEzRjdmc1NwZ1U5RmRYN3dzLTV6S3RYRUFxOjlhODU0ZmJhLTAyMTYtNDg5Zi04ZWJlLWY5OTBlNjMyNTM3Yg==",
+            "Authorization" : "Basic bS0yckV0aTV4ZlhQWDEzRjdmc1NwZ1U5RmRYN3dzLTV6S3RYRUFxOjM2YTExYzJmLWU5ZWItNGNlZi1iYzk1LTYyZDZiZjEzM2Y0Nw==",
             "Content-Type": "application/json",
         }
       })
       .then((response) => response.json())
       .then((data) => rendert(data));
     }
+
+
+
